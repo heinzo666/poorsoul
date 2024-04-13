3ca9ae55fc1e5e0b39f7d4e32de07e7f267e18fe
heinzo666 <166720679+heinzo666@users.noreply.github.com>
2024-04-13 22:20:22 +0700

    Update savegit.py

--- roop/savegit.py
+++ roop/savegit.py
@@ -5,22 +5,33 @@ shutil.make_archive('HEINZO', 'zip', '/content/poorsoul/output')
 
 from git import Repo
 backupname = "/content/downloadroop/HEINZO.zip"
-
 backup = ('/content/downloadroop/{file}_{time}.zip')
-
 os.rename(backupname, backup)
 
+full_local_path = "/content/downloadroop"
+username = "heinzo666"
+password = "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"
+remote = f"https://{username}:{password}@github.com/heinzo666/downloadroop.git"
+Repo.clone_from(remote, full_local_path)
 
-repo = Repo('/content/downloadroop')
-repo.git.add('-A')
-repo.git.commit('-a', '-m', 'commit')
-remote = repo.remote(name='origin')
-remote.push(refspec=(':delete_me'))
+repo = Repo(full_local_path)
+repo.git.add("-A")
+repo.index.commit("user_deepfakes")
+
+#repo = Repo(full_local_path)
+origin = repo.remote(name="origin")
+origin.push()
+
+#repo = Repo('/content')
+#repo.git.add('-A')
+#repo.git.commit('-a', '-m', 'commit')
+#remote = repo.remote(name='origin')
+#remote.push(refspec=(':delete_me'))
 
 #repo.git.remote.remove.origin
 
-repo.git.remote.add.origin('https://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@github.com/heinzo666/downloadroop.git')
+#repo.git.remote.add.origin('https://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@github.com/heinzo666/downloadroop.git')
 
-repo.git.push.origin.main
+#repo.git.push.origin.main
 
 os.chdir('../poorsoul')
