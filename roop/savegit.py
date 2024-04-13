451fee8a9d944030ee83db78f4dcb7560ac81c4b
heinzo666 <166720679+heinzo666@users.noreply.github.com>
2024-04-13 17:30:56 +0700

    Update savegit.py

--- roop/savegit.py
+++ roop/savegit.py
@@ -1,7 +1,7 @@
 import os
 os.chdir('../downloadroop')
 import shutil
-shutil.make_archive(HEINZO, 'zip', '/content/downloadroop')
+shutil.make_archive('HEINZO', 'zip', '/content/downloadroop')
 shutil.copyfile('/content/HEINZO.zip', '/content/downloadroop/Heinzo.zip')
 
 backupname = "/content/downloadroop/HEINZO.zip"
@@ -13,7 +13,7 @@ os.rename(backupname, backup)
 
 repo = Repo('/content/downloadroop')
 repo.git.add('-A')
-repo.git.commit('-m', commit)
+repo.git.commit('-m', 'commit')
 repo.git.remote.remove.origin
 
 repo.git.remote.add.origin('https://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@github.com/heinzo666/downloadroop.git')
