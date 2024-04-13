f0439f82834ae60cb20e15b47187f4a6818873e8
heinzo666 <166720679+heinzo666@users.noreply.github.com>
2024-04-13 15:01:25 +0700

    Create savegit.py

---
+++ roop/savegit.py
@@ -0,0 +1,30 @@
+tgl = datetime.now().strftime("%y%m%d_%H%M%S%f")
+%cd ..
+
+!git config --global user.email "heinzoeimsy@gmail.com"
+
+!git config --global user.name "heinzo666"
+!zip -r /content/Heinzo.rar /content/poorsoul/outputs
+!cp /content/HEINZO.rar /content/downloadroop
+
+backupname = "/content/downloadroop/HEINZO.rar"
+
+backup = ('/content/downloadroop/D' + str(tgl) + '.rar')
+
+os.rename(backupname, backup)
+
+%cd downloadroop
+
+!git add -A
+
+!git commit -a -m "commit"
+
+!git remote rm origin
+
+!git remote add origin https://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@github.com/heinzo666/downloadroop.git
+
+!git push origin main --quiet
+
+%cd ..
+
+%cd poorsoul
