52dbe65b246c4831fc9267928bdf4c9f62d7594e
heinzo666 <166720679+heinzo666@users.noreply.github.com>
2024-04-14 08:07:32 +0700

    Update savegit.py

--- roop/savegit.py
+++ roop/savegit.py
@@ -2,7 +2,7 @@ import os
 from git import Repo
 full_local_path = "/content/downloadroop"
 username = "heinzo666"
-password = "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"
+password = "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"
 remote = f"https://{username}:{password}@github.com/heinzo666/downloadroop.git"
 Repo.clone_from(remote, full_local_path)
 
